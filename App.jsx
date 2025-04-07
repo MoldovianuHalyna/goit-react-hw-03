a7da17ca7795c02fc3869611eefa06895897ba85
Halyna Moldovianu <Beorgeos@gmail.com>
2025-04-07 19:24:49 +0200

    1

--- App.jsx
+++ App.jsx
@@ -7,17 +7,26 @@ import { ToastContainer, toast } from "react-toastify";
 import { Slide } from "react-toastify";
 import { MdDeleteOutline } from "react-icons/md";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import s from "./main.module.css";
 
 function App() {
-  const [contacts, setContacts] = useState([
-    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ]);
+  const [contacts, setContacts] = useState([]);
   const [filter, setFilter] = useState("");
+  useEffect(() => {
+    const savedContacts = localStorage.getItem("contacts");
+    if (savedContacts) {
+      setContacts(JSON.parse(savedContacts));
+    } else {
+      setContacts([
+        { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+        { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+        { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+        { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+      ]);
+    }
+  }, []);
+
   const handleChangeInput = (event) => {
     const { value } = event.target;
     setFilter(value);
@@ -28,10 +37,14 @@ function App() {
   );
 
   const addContact = (contact) => {
-    setContacts([...contacts, contact]);
+    const newContacts = [...contacts, contact];
+    setContacts(newContacts);
+    localStorage.setItem("contacts", JSON.stringify(newContacts));
   };
   const deleteContact = (id) => {
-    setContacts(contacts.filter((contact) => contact.id !== id));
+    const newContacts = contacts.filter((contact) => contact.id !== id);
+    setContacts(newContacts);
+    localStorage.setItem("contacts", JSON.stringify(newContacts));
     toast.success("Contact deleted successfully", {
       position: "top-center",
       autoClose: 3000,
